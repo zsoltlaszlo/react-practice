011bcb7cc1bf3cb29aaa2bdc51e1c3aec432b84f
Zsolt <zsoltlaszlo1994@gmail.com>
2019-06-05 12:04:29 +0200

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import MainConent from "./components/MainContent"
+import Footer from "./components/Footer"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <MainConent />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <h1>Navbar component works</h1>
+    )    
+}
+
+export default Navbar
